174e9c2fd77b9fee2df2240bde575a787f33a465
Lekshmanan <lekshmanan27102004@gmail.com>
2025-09-07 15:31:57 +0530

    Your commit message here

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,10 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Verify from './pages/Verify/Verify'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import Footer from './components/Footer/Footer';
+import MyOrders from './pages/MyOrders/MyOrders';
 
 const App = () => {
 
@@ -20,6 +22,8 @@ const [showLogin,setShowLogin]=useState(false)
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/order' element={<PlaceOrder />} />
+        <Route path='/Verify' element={<Verify/>} />
+        <Route path='/myorders' element={<MyOrders/>} />
       </Routes>
     </div>
     <Footer />
