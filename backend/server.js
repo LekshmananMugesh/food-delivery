4e354221da4c001099235518649d3dfe9d010fc3
Lekshmanan <lekshmanan27102004@gmail.com>
2025-10-13 19:49:55 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
